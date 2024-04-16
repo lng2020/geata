51e2c79337840c0e95a6473ac84d23fe6fb35565
lng2020 <nanguanlin6@gmail.com>
2024-04-16 19:15:25 +0800

    Remove toggle option from sidebar

--- web/src/components/Sidebar.vue
+++ web/src/components/Sidebar.vue
@@ -1,71 +1,33 @@
 <template>
-  <div class="h-screen bg-indigo-950" :class="sidebarVisible ? 'w-72' : 'w-15'">
-    <div class="mb-2 relative flex items-center">
-      <img src="../assets/gopher.png" alt="logo" class="w-10 h-10 m-4 duration-300" />
-      <span class="text-white text-2xl font-extrabold" :class="{ hidden: !sidebarVisible }"
-        >Geata</span
-      >
-      <button @click="sidebarVisible = !sidebarVisible">
-        <font-awesome-icon
-          icon="fa-solid fa-arrow-left"
-          class="bg-white rounded-full text-3xl absolute -right-3 top-9 border border-indigo-700 cursor-pointer"
-          :class="{ 'rotate-180': !sidebarVisible }"
-        ></font-awesome-icon>
-      </button>
-    </div>
-    <ul>
-      <li class="mb-4 flex items-center">
-        <font-awesome-icon
-          icon="fa-solid fa-home"
-          class="text-white text-2xl ml-6 mr-4"
-        ></font-awesome-icon>
-        <router-link to="/" class="text-white text-xl ml-2" :class="{ hidden: !sidebarVisible }"
-          >Dashboard</router-link
-        >
-      </li>
-      <li v-for="(station, index) in stations" :key="index" @click="toggleOptions(index)">
-        <div class="mb-4 flex items-center">
-          <font-awesome-icon
-            icon="fa-solid fa-charging-station"
-            class="text-white text-2xl ml-6 mr-4"
-          >
-          </font-awesome-icon>
-          <div :class="{ hidden: !sidebarVisible }">
-            <span class="text-white text-xl ml-2">{{ station.name }}</span>
-            <font-awesome-icon
-              icon="fa-solid fa-chevron-down"
-              class="text-white text -right-4"
-              :class="{ 'rotate-180': !station.showOptions }"
-            ></font-awesome-icon>
-          </div>
-        </div>
-        <div :class="{ hidden: !sidebarVisible || !station.showOptions }">
-          <ul v-show="station.showOptions">
-            <li class="text-white text ml-32 mb-4">
-              <router-link :to="{ path: '/station/' + station.id }">Overview</router-link>
-            </li>
-            <li class="text-white text ml-32 mb-4">
-              <router-link :to="{ path: '/mapping/' + station.id }">Mapping</router-link>
-            </li>
-            <li class="text-white text ml-32 mb-4">
-              <router-link :to="{ path: '/setting/' + station.id }">Setting</router-link>
-            </li>
-          </ul>
-        </div>
-      </li>
-    </ul>
+  <div class="h-screen w-64 bg-gray-800">
+    <div class="flex items-center px-4 py-3"> <img src="../assets/gopher.png" alt="logo" class="w-8 h-8 mr-2" /> <span
+        class="text-white text-xl font-bold">Geata</span> </div>
+    <nav class="mt-6"> <router-link to="/" class="flex items-center px-4 py-2 text-white hover:bg-gray-700">
+        <font-awesome-icon icon="fa-solid fa-home" class="text-lg mr-2"></font-awesome-icon> <span>Dashboard</span>
+      </router-link>
+      <div v-for="(station, index) in stations" :key="index" class="mt-4"> <button @click="toggleOptions(index)"
+          class="flex items-center justify-between w-full px-4 py-2 text-white hover:bg-gray-700">
+          <div class="flex items-center"> <font-awesome-icon icon="fa-solid fa-charging-station"
+              class="text-lg mr-2"></font-awesome-icon> <span>{{ station.name }}</span> </div> <font-awesome-icon
+            :icon="station.showOptions ? 'fa-solid fa-chevron-up' : 'fa-solid fa-chevron-down'"
+            class="text-sm"></font-awesome-icon>
+        </button>
+        <div v-show="station.showOptions" class="mt-2 space-y-2 pl-12"> <router-link
+            :to="{ path: '/station/' + station.id }" class="block text-sm text-gray-400 hover:text-white"> Overview
+          </router-link> <router-link :to="{ path: '/mapping/' + station.id }"
+            class="block text-sm text-gray-400 hover:text-white"> Mapping </router-link> <router-link
+            :to="{ path: '/setting/' + station.id }" class="block text-sm text-gray-400 hover:text-white"> Setting
+          </router-link> </div>
+      </div>
+    </nav>
   </div>
 </template>
-
 <script lang="ts" setup>
 import { userGlobalStore } from '@/stores/store'
-import { ref, reactive } from 'vue'
+import { reactive } from 'vue'
 const store = userGlobalStore()
-let stations = reactive(store.stations)
-let sidebarVisible = ref(store.sidebarVisible)
+const stations = reactive(store.stations)
 const toggleOptions = (index: number) => {
   stations[index].showOptions = !stations[index].showOptions
 }
 </script>
-
-<style scoped></style>
