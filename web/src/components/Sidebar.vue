69280c5f5893f1587085e4c6f22751ccd58a127c
lng2020 <nanguanlin6@gmail.com>
2024-05-25 19:57:17 +0800

    Improve sidebar

--- web/src/components/Sidebar.vue
+++ web/src/components/Sidebar.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="h-screen w-64 bg-gray-800">
-    <div class="flex items-center px-4 py-3 bg-gray-900">
-      <img src="../assets/gopher.png" alt="logo" class="w-8 h-8 mr-2" />
+    <div class="flex items-center px-4 py-4 bg-gray-900">
+      <img src="../assets/gopher.png" alt="logo" class="w-8 h-8 mr-3" />
       <span class="text-white text-xl font-bold">Geata</span>
     </div>
     <nav class="mt-6">
-      <router-link to="/" class="flex items-center px-4 py-2 text-white hover:bg-gray-700">
-        <font-awesome-icon icon="fa-solid fa-home" class="text-lg mr-2"></font-awesome-icon>
+      <router-link to="/" class="flex items-center px-5 py-3 text-white hover:bg-gray-700">
+        <font-awesome-icon icon="fa-solid fa-home" class="text-lg mr-3"></font-awesome-icon>
         <span>Dashboard</span>
       </router-link>
       <div v-for="(station, index) in stations" :key="index" class="mt-4">
         <button
           @click="toggleOptions(index)"
-          class="flex items-center justify-between w-full px-4 py-2 text-white hover:bg-gray-700"
+          class="flex items-center justify-between w-full px-5 py-3 text-white hover:bg-gray-700" 
         >
           <div class="flex items-center">
             <font-awesome-icon
               icon="fa-solid fa-charging-station"
-              class="text-lg mr-2"
-            ></font-awesome-icon>
+              class="text-lg mr-3"
+            ></font-awesome-icon> 
             <span>{{ station.name }}</span>
           </div>
           <font-awesome-icon
@@ -26,7 +26,7 @@
             class="text-sm"
           ></font-awesome-icon>
         </button>
-        <div v-show="station.showOptions" class="mt-2 space-y-2 pl-12 bg-gray-700">
+        <div v-show="station.showOptions" class="mt-2 pt-2 pb-2 bg-gray-700 flex flex-col items-center justify-center space-y-2">
           <router-link
             :to="{ path: '/station/' + station.id }"
             class="block text-sm text-gray-400 hover:text-white"
